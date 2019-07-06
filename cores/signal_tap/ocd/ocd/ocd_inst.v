aaf53c4e9d8b4616e88684ee0ef5440f1c132cd2
PulseRainmaker <pulserain@pulserain.com>
2019-07-05 22:23:09 -0700

    remove unused sigtap

--- cores/signal_tap/ocd/ocd/ocd_inst.v
+++
@@ -1,6 +0,0 @@
-	ocd u0 (
-		.acq_data_in    (<connected-to-acq_data_in>),    //     tap.acq_data_in
-		.acq_trigger_in (<connected-to-acq_trigger_in>), //        .acq_trigger_in
-		.acq_clk        (<connected-to-acq_clk>)         // acq_clk.clk
-	);
-
